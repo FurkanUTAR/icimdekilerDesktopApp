d6e5afc3cecd770ffdeb5acb2a05fc3e60c7b3b6
MustafaCanKaya335 <mustfcankya09@gmail.com>
2025-02-09 15:39:57 +0300

    Hata Düzeltmeleri Yapıldı

--- icimdekiler/Program.cs
+++ icimdekiler/Program.cs
@@ -16,7 +16,7 @@ namespace icimdekiler
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new adminAnaSayfa());
+            Application.Run(new Form1());
         }
     }
 }
